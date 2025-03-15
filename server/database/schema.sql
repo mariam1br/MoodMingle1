54f62fb186b3816e925f912b3e35104a1119228a
Junaid Khan <junaid.khan1@ucalgary.ca>
2025-03-14 23:32:45 -0600

    Pull updated Schema and testingDb from main

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -8,7 +8,6 @@ CREATE TABLE Users (
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    location VARCHAR(255) NOT NULL,
     createdAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -22,52 +21,10 @@ CREATE TABLE Activities (
     activityID SERIAL PRIMARY KEY,
     userID BIGINT UNSIGNED NOT NULL,
     name VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL,
+    genre VARCHAR(255) NOT NULL,
     location VARCHAR(255) NOT NULL,
     weather VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (userID) REFERENCES Users(userID) ON DELETE CASCADE
 );
-
-CREATE TABLE Weather (
-    weatherID SERIAL PRIMARY KEY,  
-    location VARCHAR(255) NOT NULL,  
-    Date DATE NOT NULL,  
-    temperature DECIMAL(5, 2) NOT NULL,  
-    conditions VARCHAR(255) NOT NULL  
-);
-
-CREATE TABLE LocalEvents (
-    eventID SERIAL PRIMARY KEY,  
-    name VARCHAR(255) NOT NULL,  
-    location VARCHAR(255) NOT NULL, 
-    description TEXT NOT NULL,  
-    tags TEXT  -- Tags or categories for the event (e.g., Comics, Music)
-);
-
-CREATE TABLE Recommendations (
-    recommendationID SERIAL PRIMARY KEY,  
-    userID BIGINT UNSIGNED NOT NULL,  
-    activityID BIGINT UNSIGNED NOT NULL,  
-    weatherCondition VARCHAR(255) NOT NULL,  
-    FOREIGN KEY (userID) REFERENCES Users(userID) ON DELETE CASCADE,  
-    FOREIGN KEY (activityID) REFERENCES Activities(activityID) ON DELETE CASCADE  
-);
-
-CREATE TABLE GoogleGeminiInteractions (
-    interactionID SERIAL PRIMARY KEY,  
-    userID BIGINT UNSIGNED NOT NULL,  
-    query TEXT NOT NULL,  
-    response TEXT NOT NULL,  
-    FOREIGN KEY (userID) REFERENCES Users(userID) ON DELETE CASCADE  
-);
-
-CREATE TABLE APILogs (
-    logID SERIAL PRIMARY KEY,  
-    APIName VARCHAR(255) NOT NULL,  
-    requestPayload JSON NOT NULL,  
-    responsePayload JSON NOT NULL,  
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP  
-);
-
