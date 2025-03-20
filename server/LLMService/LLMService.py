e48bbeb835e916dfc260c5a75f8f89438a105d0d
mariam1br <mariam.ibrahim1@ucalgary.ca>
2025-03-19 23:22:50 -0600

    cors

--- server/LLMService/LLMService.py
+++ server/LLMService/LLMService.py
@@ -9,7 +9,7 @@ import google.generativeai as genai
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, origins=["https://moodmingle-2mfi.onrender.com"])
+CORS(app, origins=["https://moodmingle-1w1q.onrender.com"])
 
 GEMINI_API_KEY = os.getenv("LLM_API_KEY")
 
